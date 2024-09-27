ba0934963e47fb306e9ac71813bced396d676cc1
AdriaVH <adriavargashernandez@gmail.com>
2024-09-27 13:05:16 +0200

    front deployed

--- Front/src/stores/karaoke.js
+++ Front/src/stores/karaoke.js
@@ -15,7 +15,7 @@ export const useKaraokeStore = defineStore({
     songsOnCart:[],
     visible:true,
     tunnel:"",
-    localhost:"https://karaokelist-6z1w.onrender.com/"
+    localhost:"https://karaokelist-6z1w.onrender.com"
 
 
   
